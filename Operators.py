6773eb00a02dca04d3975a5b47182176a297cc93
SomeRandomPerson4112 <ayaangadani@gmail.com>
2024-11-23 18:52:40 +0500

    main.034930590958937593573973573793759357957

---
+++ Operators.py
@@ -0,0 +1,13 @@
+a=5
+b=6
+c=4
+d=1
+x=0
+x=(a+b-c)*d/a
+print("value of(a+b-c)*d/a is ",x)
+name="Ayaan"
+age=0
+if (name=="Ayaan"or name=="jonh") and age>=2:
+    print("Hello Welcome")
+else:
+    print("Goodbye")
